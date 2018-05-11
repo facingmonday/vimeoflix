e488ebf49e64b2b9b43de3c3db82a8dd081e0aec
Jason Price <jprice@airsquirrels.com>
2018-05-11 14:20:59 -0400

    Updating config

--- server.js
+++ server.js
@@ -8,4 +8,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(3020);
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
